ba411bb1a638d4669dfc566461fc08bdb1b37497
Ivan <ivanjose_04@hotmail.com>
2022-09-09 15:59:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const getVideos = require('./services/notion')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/videos', async (req, res) => {
+	const videos = await getVideos()
+	res.json(videos)
+})
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
+console.log('http://localhost:5000')
+
+
+/* ;(async () => {
+	const nVideos = await getVideos()
+	console.log(nVideos);
+})()
+ */
